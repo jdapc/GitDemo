c9e503bf465300c789be994d3b850c0c573b975a
Johan <invitado@s1pc17.eisc.univalle.edu.co>
2023-03-30 16:41:00 -0500

    Change Person Class

--- src/model/Person.java
+++ src/model/Person.java
@@ -11,10 +11,12 @@ package model;
 public class Person {
     private int id;
     private String name;
+    private String lastName;
 
-    public Person(int id, String name) {
+    public Person(int id, String name, String lastName) {
         this.id = id;
         this.name = name;
+        this.lastName = lastName;
     }
 
     public int getId() {
@@ -29,7 +31,22 @@ public class Person {
         return name;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + '}';
+    }
+
+   
 }
